ae7a349a858f8607bde4449496e435548b08472b
peer2 <charles@peer2.com>
2012-10-27 17:27:00 -0700

    Update lib/panoramic/resolver.rb
    removing cache clearing for testing

--- lib/panoramic/resolver.rb
+++ lib/panoramic/resolver.rb
@@ -80,13 +80,13 @@ module Panoramic
     def clear_cache_if_necessary
       logger = Logger.new(STDERR)
       # logger.debug "Panoramic clear_cache_if_necessary"
-      last_updated = Rails.cache.fetch("panoramic_stored_template_last_updated") { Time.now }
+      #last_updated = Rails.cache.fetch("panoramic_stored_template_last_updated") { Time.now }
       # logger.debug "Panoramic CACHE - Last Updated (from memcache): #{last_updated.inspect} - cache_last_updated value: #{@cache_last_updated.inspect}"
-      if @cache_last_updated.nil? || @cache_last_updated < last_updated
+     # if @cache_last_updated.nil? || @cache_last_updated < last_updated
         # logger.debug "View Cache needs to be wiped"
-        clear_cache
-        @cache_last_updated = last_updated
-      end
+     #   clear_cache
+     #   @cache_last_updated = last_updated
+     # end
     end
 
   end
